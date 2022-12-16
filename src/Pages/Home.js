4f69478c55e4095c91391b60ed0f94e281a7145c
Dalibor Drevojanek <drevojanekdalibor@gmail.com>
2022-12-16 16:14:24 +0100

    init

---
+++ src/Pages/Home.js
@@ -0,0 +1,9 @@
+function Home() {
+  return (
+    <div>
+      <h1>OSU QUEST</h1>
+    </div>
+  );
+}
+
+export default Home;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import Home from './Pages/Home';
+import App from './Pages/App';
+
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+const router = createBrowserRouter([
+  {
+      path: "/",
+      element: <Home />,
+  },
+]);
+
+
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <RouterProvider router={router} fallbackElement={<App />}></RouterProvider>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
