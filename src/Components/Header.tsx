fde63c75cedc57bbd30de0eadb4c09abffb5fb5f
Dalibor Drevojanek <drevojanekdalibor@gmail.com>
2022-12-18 01:54:31 +0100

    Add "logged in as" text

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -13,7 +13,10 @@ export default function Header() {
 		<div>
 			{!state.user && <a href="https://osu.ppy.sh/oauth/authorize?response_type=code&client_id=19415&redirect_uri=http://localhost:3000/login&scope=public">Login / Register</a>}
 			{state.user && (
-				<button onClick={handleClick}>Log out</button>
+				<>
+					<p>You're currently logged in as: {state.user.username.toUpperCase()}</p>
+					<button onClick={handleClick}>Log out</button>
+				</>
 			)}
 			<br></br>
 		</div>
